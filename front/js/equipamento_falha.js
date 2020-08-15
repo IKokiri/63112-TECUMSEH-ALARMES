b37648e0279834842d899383163ba39779f50914
Luiz Mendes <developer.luizmendes@gmail.com>
2020-08-15 14:39:46 -0300

    crud de falhas no equipamento finalizado

---
+++ front/js/equipamento_falha.js
@@ -0,0 +1,261 @@
+const controller = "EquipamentoFalhaController"
+const EquipamentoController = "EquipamentoController"
+const FalhaController = "FalhaController"
+
+$(document).ready(function(){
+
+inicio();
+
+function inicio(){
+    grid_principal();
+    carregar_selects();
+    limpar_campos()
+    $('#modal_principal').modal('hide')
+}
+
+function carregar_selects(){
+    load_falhas();
+    load_equipamentos();
+}
+
+function load_falhas(){
+
+    formData = new FormData();
+    formData.append('class', FalhaController);
+    formData.append('method', 'read');
+
+    fetch(base_request,{
+        method:'post',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {        
+        options = ""
+        dados = data.result_array
+        for(linha in dados){
+            options += `
+            <option value="${dados[linha].id}">
+            ${dados[linha].tag} - ${dados[linha].falha}
+            </option>
+            `
+        }
+        document.querySelector("#id_falha").innerHTML = options
+    })
+    .catch(console.error);
+}
+
+function load_equipamentos(){
+
+    formData = new FormData();
+    formData.append('class', EquipamentoController);
+    formData.append('method', 'read');
+
+    fetch(base_request,{
+        method:'post',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {        
+        options = ""
+        dados = data.result_array
+        for(linha in dados){
+            options += `
+            <option value="${dados[linha].id}">
+            ${dados[linha].tag} - ${dados[linha].equipamento}
+            </option>
+            `
+        }
+        document.querySelector("#id_equipamento").innerHTML = options
+    })
+    .catch(console.error);
+}
+
+
+function carregar_campos(){
+
+    formData = new FormData();
+    let id_equipamento = document.querySelector("#id_equipamento").value;
+    let id_falha = document.querySelector("#id_falha").value;
+    let observacao = document.querySelector("#observacao").value;
+    let id = document.querySelector("#id").value;
+
+    formData.append('id_equipamento', id_equipamento);
+    formData.append('id_falha', id_falha);
+    formData.append('observacao', observacao);
+    formData.append('id', id);
+
+    return formData;
+}
+
+function limpar_campos(){
+
+    let id_equipamento = document.querySelector("#id_equipamento").value = "";
+    let id_falha = document.querySelector("#id_falha").value = "";
+    let observacao = document.querySelector("#observacao").value = "";
+    let id = document.querySelector("#id").value = "";
+
+}
+
+function preencher_form(data){
+
+    let id_equipamento = data.id_equipamento;
+    let id_falha = data.id_falha;
+    let observacao = data.observacao;
+    let id = data.id;
+    
+    $('#modal_principal').modal('show')
+    document.querySelector("#id_equipamento").value = id_equipamento
+    document.querySelector("#id_falha").value = id_falha
+    document.querySelector("#observacao").value = observacao
+    document.querySelector("#id").value = id
+    
+}
+
+$(document).on('click','#abrir_modal',function(){
+    $('#modal_principal').modal('show')
+})
+
+$('#modal_principal').on('hidden.bs.modal', function () {
+    document.querySelector("#id").value = ""
+  })
+
+$(document).on('click','#salvar',function(){
+    formData = carregar_campos()
+    id = formData.get("id");
+
+    if(id){
+        update(formData)
+    }else{
+        criar(formData)
+    }
+
+})
+
+$(document).on('click','#remover',function(){
+    
+    id = $(this).attr("data-id");
+    
+    var res = confirm("Deseja remover o registro?");
+    if (res == true) {
+        remover(id);
+    } else {
+    
+    }
+    
+})
+
+$(document).on('click','#edit',function(){
+
+    $('#modal_principal').modal('show')
+    id = $(this).attr("data-id");
+    edit(id);
+    
+})
+
+
+function grid_principal(){
+
+    formData = new FormData();
+    formData.append('class', controller);
+    formData.append('method', 'readLazy');
+
+    fetch(base_request,{
+        method:'post',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {        
+        grid = ""
+        dados = data.result_array
+        for(linha in dados){
+            grid += 
+            `
+                <tr>
+                    <td>${dados[linha].tag_equipamento} - ${dados[linha].equipamento}</td>
+                    <td>${dados[linha].tag_falha} - ${dados[linha].falha}</td>
+                    <td>${dados[linha].observacao}</td>
+                    <td data-id="${dados[linha].id}" id="edit"><img src="./icons/001-pencil.png"  alt=""></td>
+                    <td data-id="${dados[linha].id}" id="remover"><img src="./icons/002-delete.png"  alt=""></td>
+                </tr>
+            `
+        }
+        document.querySelector(".grid").innerHTML = grid
+    })
+    .catch(console.error);
+}
+
+// CRIAR
+function criar(formData){
+    
+
+    formData = carregar_campos();
+    formData.append('class', controller);
+    formData.append('method', 'create');
+    
+        fetch(base_request,{
+            method:'post',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {        
+            inicio()
+        })
+        .catch(console.error);
+}
+
+function remover(id){
+    
+    formData = new FormData();
+    formData.append('class', controller);
+    formData.append('method', 'delete');
+    formData.append('id', id);
+    
+        fetch(base_request,{
+            method:'post',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {     
+            inicio()
+        })
+        .catch(console.error);
+}
+
+function edit(id){
+
+    formData = new FormData();
+    formData.append('class', controller);
+    formData.append('method', 'getId');
+    formData.append('id', id);
+    
+        fetch(base_request,{
+            method:'post',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {        
+            $linha = data.result_array[0];
+            preencher_form($linha);
+
+        })
+        .catch(console.error);
+}
+
+function update(formData){
+
+    formData.append('class', controller);
+    formData.append('method', 'update');
+    
+        fetch(base_request,{
+            method:'post',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {     
+            inicio()
+        })
+        .catch(console.error);
+}
+
+
+})
---
+++ src/App/Model/EquipamentoFalhaModel.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Model;
+
+use App\DAO\Database;
+use App\Core\Model;
+use PDO;
+
+class EquipamentoFalhaModel extends Model{
+
+    private $table = "`tecumsehalarmes63112`.`equipamento_falhas`";
+    private $model = "EquipamentoFalhaModel";
+
+    function read(){
+        
+        $sql = "SELECT * FROM ".$this->table;
+
+        $query = $this->conn->prepare($sql);
+
+        $result = Database::executa($query);   
+
+        $this->log->setInfo("Buscou ($this->model read) os registros");
+
+        return $result;
+
+    }
+
+    function readLazy(){
+        
+        $sql = "SELECT 
+                    equ_fal.*,
+                    equ.tag AS tag_equipamento,
+                    equ.equipamento,
+                    fal.tag AS tag_falha,
+                    fal.falha
+                FROM
+                    ".$this->table." equ_fal
+                        INNER JOIN
+                    equipamentos equ ON equ_fal.id_equipamento = equ.id
+                        INNER JOIN
+                    falhas fal ON equ_fal.id_falha = fal.id";
+
+        $query = $this->conn->prepare($sql);
+
+        $result = Database::executa($query);   
+
+        $this->log->setInfo("Buscou ($this->model read) os registros");
+
+        return $result;
+
+    }
+
+    function getId($data){
+        
+        $this->populate($data);
+
+        $sql = "SELECT * FROM ".$this->table." 
+        WHERE `id` = :id;";
+
+        $query = $this->conn->prepare($sql);
+
+        $query->bindValue(':id', $this->id, PDO::PARAM_STR);
+
+        $result = Database::executa($query);   
+
+        $this->log->setInfo("Buscou ($this->model getId) o registro $this->id");
+
+        return $result;
+    }
+
+    function create($data){
+        
+        $this->populate($data);
+        
+        $sql = "INSERT INTO ".$this->table." 
+                    (`id_equipamento`,
+                    `id_falha`,
+                    `observacao`,
+                    `criado`)
+                    VALUES
+                    (:id_equipamento,
+                    :id_falha,
+                    :observacao,
+                    curtime())";
+
+        $query = $this->conn->prepare($sql);
+        
+        $query->bindValue(':id_equipamento', $this->id_equipamento, PDO::PARAM_STR);
+        $query->bindValue(':id_falha', $this->id_falha, PDO::PARAM_STR);
+        $query->bindValue(':observacao', $this->observacao, PDO::PARAM_STR);
+        
+        $result = Database::executa($query); 
+          
+        $this->log->setInfo("Criou ($this->model create) o registro ". $this->conn->lastInsertId());
+
+        return $result;
+    }
+
+    function update($data){
+
+        $this->populate($data);
+
+        $sql = "UPDATE ".$this->table." 
+                SET
+                `id_equipamento` = :id_equipamento,
+                `id_falha` = :id_falha,
+                `observacao` = :observacao,
+                `editado` = curtime()
+                WHERE `id` = :id;";
+
+        $query = $this->conn->prepare($sql);
+        
+        $query->bindValue(':id', $this->id, PDO::PARAM_STR);
+        $query->bindValue(':id_equipamento', $this->id_equipamento, PDO::PARAM_STR);        
+        $query->bindValue(':id_falha', $this->id_falha, PDO::PARAM_STR);  
+        $query->bindValue(':observacao', $this->observacao, PDO::PARAM_STR);  
+      
+        $result = Database::executa($query);   
+
+        $this->log->setInfo("Atualizaou ($this->model update) o registro $this->id");
+
+        return $result;
+    }
+
+    function delete($data){
+
+        $this->populate($data);
+
+        $sql = "DELETE FROM ".$this->table." 
+                    WHERE `id` = :id;";
+
+        $query = $this->conn->prepare($sql);
+        
+        $query->bindValue(':id', $this->id, PDO::PARAM_STR);
+
+        $result = Database::executa($query);   
+
+        $this->log->setInfo("Removeu ($this->model delete) o registro $this->id");
+        
+        return $result;
+    }
+
+
+}
+
