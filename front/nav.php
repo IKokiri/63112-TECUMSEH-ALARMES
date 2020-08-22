e3a66dcb4502ae47e9883fcc70b61c8f85d450da
Luiz Mendes <developer.luizmendes@gmail.com>
2020-08-22 13:54:43 -0300

    adição global de cursor pointer para edit e delete

--- front/nav.php
+++ front/nav.php
@@ -5,7 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="stylesheet" href="./assets/bootstrap/bootstrap.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    
+    <style>
+      #edit{
+        cursor:pointer;
+      }
+      #remover{
+        cursor:pointer;
+      }
+    </style>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
